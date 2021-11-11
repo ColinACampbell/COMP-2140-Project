db912dc357c8d0f0e501e866dcca8ceed4267718
Colin Campbell <prontobol@gmail.com>
2021-11-11 16:38:24 -0500

    Completed auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('./../controller/user')
 
 router.post('/',userController.register)
+router.post('/authenticate',userController.authenticate)
 
 module.exports = router
