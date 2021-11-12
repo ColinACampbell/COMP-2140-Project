ecfc593ff9d5e33ffd077bcf35301a4ce11653ed
Colin Campbell <prontobol@gmail.com>
2021-11-11 19:39:17 -0500

    Added position

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -23,14 +23,14 @@ exports.authenticate = (req, res) => {
 }
 
 exports.register = (req, res) => {
-    const { email, name, password } = req.body;
+    const { email, name, password, position } = req.body;
     // find if email first
     User.exists({ email }, async (err, exisits) => {
         if (exisits)
             res.status(409).json({})
         else {
             const hashedPassword = passwordUtil.createPasswordHash(password)
-            const user = await User.create({ email, name, password: hashedPassword })
+            const user = await User.create({ email, name, password: hashedPassword, position })
             user.password = undefined
             const token = jwtUtil.createToken({ ...user })
             res.status(201).json({ user, token })
