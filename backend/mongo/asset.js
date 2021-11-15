9828040a0188129258278a87677550eb5bfb9bb5
Colin Campbell <prontobol@gmail.com>
2021-11-15 00:59:26 -0500

    Added id field

--- backend/mongo/asset.js
+++ backend/mongo/asset.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 const mongooseConn = require('./index')
 
 const assetSchema = new Schema({
+    owner_id: String,
     name : String,
     data : String
 })
