695a511cb449ec12375a98ef02fdfc1bf6bfb6fd
Colin Campbell <prontobol@gmail.com>
2021-11-17 23:52:07 -0500

    Get asset

--- backend/controller/asset.js
+++ backend/controller/asset.js
@@ -1,33 +1,38 @@
-const fs = require("fs");
-const assetModel = require("../mongo/asset");
+const crypto = require('crypto');
+const Asset = require("../mongo/asset");
 //const {mangoose }= require("mangoose");
 exports.getAssets = (req, res) => {
-    res.status(200).json({
-        assets: []
+    Asset.find({},'title type description sender').then((assets) => {
+        res.status(200).json(assets)
     })
 }
+
+exports.getAsset = (req, res) => {
+    // TODO : Implement
+}
+
 exports.uploadAsset = (req, res) => {
-    // SET STORAGE
-    //const upload = multer({storage: storage});
-    console.log(req.body)
-    let asset = {
-        // attributes of the document and should correspond with mango
-        file: req.body.encodedFile,
-        type: req.body.type,
-        title: req.body.title,
-        description: req.body.description,
-        sender: req.body.sender, // the id of the sender
-        link: req.body.link, // this should be auto generated
-        recepient: req.body.recepient // the id of the recipient
-    }
-    assetModel.create(asset, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(500).json({})
-        } else {
-            console.log("Saved To database");
-            res.send(201); // Send the '201' status code to let the user/client know the operation was successful
+
+    crypto.randomBytes(48, (err, buffer) => {
+        const tokenLink = buffer.toString("hex");
+        let asset = {
+            // attributes of the document and should correspond with mango
+            file: req.body.encodedFile, // base64 format
+            type: req.body.type,
+            title: req.body.title,
+            description: req.body.description,
+            sender: req.body.sender, // the id of the sender
+            link: tokenLink, // this should be auto generated
+            recepient: req.body.recepient // the id of the recipient
         }
+        Asset.create(asset, function (err, result) {
+            if (err) {
+                if (err) throw err;
+                res.status(500).json({})
+            } else {
+                res.status(201).json({}); // Send the '201' status code to let the user/client know the operation was successful
+            }
+        })
     })
 }
 // 1. get asset related to a user. i.e a person who sent the asset or in the receipent list.
