7b7edbf3ed1c650886097aee5d95f2dfda36fa1f
Colin Campbell <prontobol@gmail.com>
2021-12-01 14:04:29 -0500

    Function

--- backend/controller/meeting-alert.js
+++ backend/controller/meeting-alert.js
@@ -1,19 +1,33 @@
 const MeetingAlert = require('./../mongo/meeting-alert')
 
-exports.createMeetingAlert = (req, res) => {
+const duplicateCheck = async (res, title, createMeetingAlert) => {
+    const existingMeetingAlerts = await MeetingAlert.find({
+        title: title
+    });
+
+    if (existingMeetingAlerts > 0)
+        res.status(409).json({})
+    else
+        createMeetingAlert(); // runs the create
+}
+
+exports.createMeetingAlert = async (req, res) => {
     const { attendees, meetingLink, date, title, sender } = req.body;
     // Write code to check if any inputs are valid
-    MeetingAlert.create({
-        title,
-        sender,
-        attendees,
-        meetingLink,
-        date
-    }).then((meetingAlert) => {
-        res.status(201).json(meetingAlert)
-    }).catch((err) => {
-        res.status(500).json({})
+    duplicateCheck(res, title, () => {
+        MeetingAlert.create({
+            title,
+            sender,
+            attendees,
+            meetingLink,
+            date
+        }).then((meetingAlert) => {
+            res.status(201).json(meetingAlert)
+        }).catch((err) => {
+            res.status(500).json({})
+        })
     })
+
 }
 
 exports.getMeetingAlerts = (req, res) => {
@@ -50,8 +64,8 @@ exports.editMeetingAlert = (req, res) => {
 exports.deleteMeetingAlert = (req, res) => {
     const meetingId = req.params._id;
     MeetingAlert.deleteOne({
-        id:meetingId
-    },()=>{
-        res.status(200).json({message:"Item was deleted"})
+        id: meetingId
+    }, () => {
+        res.status(200).json({ message: "Item was deleted" })
     })
 }
